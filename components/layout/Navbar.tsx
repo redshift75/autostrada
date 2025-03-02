4cefc2c3fcdb84d3004554821f6a9c439b9627cb
redshift75 <49161675+redshift75@users.noreply.github.com>
2025-03-02 15:12:29 -0500

    navbar title tweak

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
                   : 'text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700'
               }`}
             >
-              Auctions
+              Auction Results
             </Link>
             <Link 
               href="/listings" 
