144707e04ddb8be824df62705c6791dfccce7dbd
redshift75 <49161675+redshift75@users.noreply.github.com>
2025-03-22 13:40:51 -0400

    mobile layout improved

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -166,6 +166,14 @@ export default function Navbar() {
           >
             Deal Finder
           </Link>
+          <div className={`block px-3 py-2 rounded-md text-base font-medium`}>
+          <SignedOut>
+              <SignInButton><button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</button></SignInButton>
+          </SignedOut>
+          <SignedIn>
+              <UserButton />
+          </SignedIn>
+          </div>
         </div>
       </div>
     </nav>
