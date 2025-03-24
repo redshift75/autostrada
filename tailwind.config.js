7c34ebe0fef1a1db415c69706e73b8a743a508f6
redshift75 <49161675+redshift75@users.noreply.github.com>
2025-03-24 17:45:33 -0400

    css tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,20 @@ module.exports = {
         'fade-in': {
           '0%': { opacity: '0', transform: 'translateY(10px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' }
-        }
+        },
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
       },
       animation: {
-        'fade-in': 'fade-in 0.6s ease-out'
+        'fade-in': 'fade-in 0.6s ease-out',
+        'accordion-down': 'accordion-down 0.3s ease-out',
+        'accordion-up': 'accordion-up 0.3s ease-out',
       }
     }
   },
